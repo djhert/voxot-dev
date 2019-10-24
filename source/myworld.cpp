03746d3da85976a9a1049be3f2a0a279d87d675d
David Hertenstein <davidh@inmotionhosting.com>
2019-10-24 17:43:03 -0400

    bump to voxot v0.3.0

--- source/myworld.cpp
+++ source/myworld.cpp
@@ -6,10 +6,21 @@ myworld::myworld() {}
 myworld::~myworld() {}
 
 void myworld::Generate() {
-	this->add_child(Voxot::Chunk::New<Voxot::Chunk>(this, 0, 0, 0));
-	this->add_child(Voxot::Chunk::New<Voxot::Chunk>(this, ChunkWidth, 0, 0));
-	this->add_child(Voxot::Chunk::New<Voxot::Chunk>(this, 0, 0, ChunkDepth));
-	this->add_child(Voxot::Chunk::New<Voxot::Chunk>(this, ChunkWidth, 0, ChunkDepth));
+	Voxot::Chunk *ch = _NewChunk("default");
+	ch->setup(this, 0, 0, 0);
+	this->add_child(ch);
+
+	ch = _NewChunk("default");
+	ch->setup(this, ChunkWidth, 0, 0);
+	this->add_child(ch);
+
+	ch = _NewChunk("default");
+	ch->setup(this, 0, 0, ChunkDepth);
+	this->add_child(ch);
+
+	ch = _NewChunk("default");
+	ch->setup(this, ChunkWidth, 0, ChunkDepth);
+	this->add_child(ch);
 }
 
 void myworld::_register_methods() {
