2614235e6cd351549d613fa2189dc728cfe1541c
hlfstr <david.hertenstein@gmail.com>
2019-10-30 04:16:46 -0400

    lots of memory improvements, is about 70% faster now

--- source/myworld.cpp
+++ source/myworld.cpp
@@ -6,11 +6,10 @@ myworld::myworld() {}
 myworld::~myworld() {}
 
 void myworld::Generate() {
-	Voxot::Chunk *ch = _NewChunk("default");
-	ch->setup(this, 0, 0, 0);
-	this->add_child(ch);
+	//Voxot::Chunk *ch = Voxot::Chunk::New<Voxot::Chunk>(this, 0, 0, 0);
+	//this->add_child(ch);
 
-	ch = _NewChunk("default");
+	Voxot::Chunk *ch = _NewChunk("default");
 	ch->setup(this, ChunkWidth, 0, 0);
 	this->add_child(ch);
 
